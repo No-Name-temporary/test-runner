7e143b9d1c1dc4d2c2f805b2ace66e7c61a45492
Tim Dronkers <tdronkers@gmail.com>
2022-07-18 20:56:16 -0600

    minor fix to trigger new build and deploy gh-action

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ exports.handler = async (event) => {
   const currRegion = event.Records[0].awsRegion;
   const { test } = JSON.parse(body.Message);
 
-  console.log(`...starting test runner in ${currRegion}`);
+  console.log(`...starting test runner in ${currRegion}...`);
   console.log('SHAPE OF TEST ---> ', test);
 
   const testConfiguration = new TestConfiguration(test);
